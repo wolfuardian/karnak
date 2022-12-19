272e3a08c9b875586a1f6b571aba64810c1edfd9
eos.chen <eos.chen@nadisystem.com>
2022-12-19 18:52:11 +0800

    新增說明文件的附圖與測試腳本

---
+++ eil/docs/examples/qt_dockable_example.py
@@ -0,0 +1,13 @@
+from PySide2 import QtWidgets
+
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+
+class MyDockableButton(MayaQWidgetDockableMixin, QtWidgets.QPushButton):
+    def __init__(self):
+        super(MyDockableButton, self).__init__()
+
+        self.setText("My Button")
+
+if __name__ == "__main__":
+    button = MyDockableButton()
+    button.show(dockable=True)
