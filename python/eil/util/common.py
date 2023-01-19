4de7e059bf167e0dc8a3cb24337c47c81dacb13f
eos.chen <eos.chen@nadisystem.com>
2023-01-19 16:45:15 +0800

    重構常用函式庫

--- python/eil/util/common.py
+++ python/eil/util/common.py
@@ -1,3 +1,7 @@
+import glob
+import os
+
+
 def rebind_dict(keys, values):
     return dict(map(lambda i, j: (i, j), keys, values))
 
@@ -62,3 +66,40 @@ def get_attr_chain(obj, attr_chain):
             return get_attr_chain(getattr(obj, attrs[0]), '.'.join(attrs[1:]))
     else:
         return None
+
+
+def basename(path):
+    return os.path.basename(path)
+
+
+def basenames(paths):
+    __file_all = []
+    for path in paths:
+        __file_all.append(basename(path))
+
+    return __file_all
+
+
+def list_files(path, file_ext, __depth=5):
+    __file_all = []
+    __file_all_group = []
+    __file_string = '*'
+    __pattern = '/*'
+    __depth = 5
+    for i in range(__depth):
+        __file_all_group.append(glob.glob(os.path.join(path, '{0}.{1}'.format(__file_string, file_ext))))
+        __file_string += __pattern
+
+    for file_group in __file_all_group:
+        for file in [file for file in file_group if (basename(file) not in basenames(__file_all))]:
+            file = file.replace('\\', '/')
+            __file_all.append(file)
+
+    return __file_all
+
+
+def reload_all_mod():
+    import eil.util.common
+    import eil.util.qt
+    reload(eil.util.common)
+    reload(eil.util.qt)
